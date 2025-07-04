3198fd4f25edfc84d4ce834e68ad16a0398cb024
Shubhamtripa <shubhamtripathi6930@gmail.com>
2025-07-04 11:13:25 +0530

    2nd commit

---
+++ Sample_Project_sel/src/test/java/pac1/CreateTest.java
@@ -0,0 +1,24 @@
+package pac1;
+
+import org.testng.annotations.Test;
+
+public class CreateTest {
+	@Test
+	public void createContactTest() {
+		String URL = System.getProperty("url");
+		String Browser = System.getProperty("browser","Chrome");
+		String UserName = System.getProperty("username");
+		String Password = System.getProperty("password");
+		
+		System.out.println(URL);
+		System.out.println(Browser);
+		System.out.println(UserName);
+		System.out.println(Password);
+		System.out.println("execute createContactTest");
+	}
+
+	@Test
+	public void modifyContactTest() {
+		System.out.println("execute modify ContactTest");
+	}
+}
